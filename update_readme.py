d8c369cdc2f9d6cb7793127f0cc45dd53c4aeadd
obezeq <obezequiel@iesrafaelalberti.es>
2025-10-01 20:14:58 +0200

    Historial en el README

--- update_readme.py
+++ update_readme.py
@@ -1,13 +1,23 @@
 import subprocess
+from datetime import datetime
 
-def run_tests():
+def get_now() -> str:
+    ahora = datetime.now()
+    fecha_hora = ahora.strftime("%Y-%m-%d %H:%M")
+    return fecha_hora
+
+def run_tests() -> str:
     try:
         subprocess.check_call(["pytest", "-q"])
-        return "✅ Tests correctos"
+        return f"✅ Tests correctos"
     except subprocess.CalledProcessError:
-        return "❌ Tests fallidos"
+        return f"❌ Tests fallidos"
 
 def update_readme(status: str):
+    
+    fecha_hora = get_now()
+    linea_historial = f"- {status[:2]} {fecha_hora} - {status[2:].strip()}\n"
+    
     with open("README.md", "r", encoding="utf-8") as f:
         lines = f.readlines()
 
@@ -15,8 +25,7 @@ def update_readme(status: str):
     for line in lines:
         new_lines.append(line)
         if line.strip() == "## Estado de los tests":
-            new_lines.append(status + "\n")
-            break
+            new_lines.append(linea_historial)
 
     with open("README.md", "w", encoding="utf-8") as f:
         f.writelines(new_lines)
